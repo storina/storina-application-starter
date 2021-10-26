39b0937e77510b72531ab9cea82b317485de1dc2
a-sabagh <a.sabagh72@gmail.com>
2021-10-27 00:58:16 +0330

    add prefix to main const 1635283696

--- plugin.php
+++ plugin.php
@@ -45,7 +45,7 @@ class storina_application_init {
         define("STORINA_PDU", plugin_dir_url(__FILE__));
         define("STORINA_PDP", plugin_dir_path(__FILE__));
         define('STORINA_PLUGIN_PATH', plugin_dir_path(__FILE__));
-        define('OSA_STORAGE', plugin_dir_path(__FILE__) . "/assets/storage/");
+        define('STORINA_STORAGE', plugin_dir_path(__FILE__) . "/assets/storage/");
         define('OSA_PLUGIN_URL', plugin_dir_url(__FILE__));	
 	}
 
