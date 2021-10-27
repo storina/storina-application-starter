9bc36cafcb988a7f37192d41aeafe23c072b0651
a-sabagh <a.sabagh72@gmail.com>
2021-10-27 16:38:49 +0330

    make woocommerce custom field clusor

--- inc/woocommerce_custom_field.php
+++ inc/woocommerce_custom_field.php
@@ -1,19 +1,11 @@
 <?php
-add_filter( 'woocommerce_product_tabs', 'woo_remove_product_tabs2', 98 );
-function woo_remove_product_tabs2( $tabs ) {
-    //unset( $tabs['description'] );      	// Remove the description tab
-    //unset( $tabs['additional_information'] );  	// Remove the additional information tab
-    //unset( $tabs['seller'] );  	// Remove the additional information tab
+add_filter( 'woocommerce_product_tabs', function ( $tabs ) {
     unset( $tabs['more_seller_product'] );  	// Remove the additional information tab
     return $tabs;
-}
+} , 98 );
 
 // Display Fields
-add_action( 'woocommerce_product_options_general_product_data', 'woo_add_custom_general_fields2' );
-
-// Save Fields
-add_action( 'woocommerce_process_product_meta', 'woo_add_custom_general_fields_save2' );
-function woo_add_custom_general_fields2() {
+add_action( 'woocommerce_product_options_general_product_data', function () {
 
     global $woocommerce, $post;
 
@@ -32,12 +24,12 @@ function woo_add_custom_general_fields2() {
 
     echo '</div>';
 
-}
-function woo_add_custom_general_fields_save2( $post_id ){
+} );
 
-// Text Field
+// Save Fields
+add_action( 'woocommerce_process_product_meta', function ( $post_id ){
     $woocommerce_text_field = $_POST['_subtitle'];
     if( !empty( $woocommerce_text_field ) )
         update_post_meta( $post_id, '_subtitle', esc_attr( $woocommerce_text_field ) );
-}
+} );
 
